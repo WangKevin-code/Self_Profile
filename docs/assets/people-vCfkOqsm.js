d90b9b690dfc25044235be3f2e277779dde34501
Kevin <kevin22413180@gmail.com>
2024-02-26 20:17:14 +0800

    vite 靜態檔 路徑測試4

---
+++ docs/assets/people-vCfkOqsm.js
@@ -0,0 +1,1 @@
+const e="/Self_Profile/assets/people-mQ6TegwG.jpg";export{e as _};
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <script setup>
 import HomeCardData from '@/JsonData/HomeCardData.js'
 import { RouterLink } from 'vue-router'
+import peopleimg from '@/Images/people.jpg'
 
 const getImage = (img) => {
     console.log(import.meta.url);
-    return new URL('../Images/' + img, import.meta.url).href;;
+    return new URL('../Images/' + img, import.meta.url).href;
 }
 </script>
 
@@ -13,7 +14,7 @@ const getImage = (img) => {
 
         <RouterLink class="col" v-for="info in HomeCardData" :to="'/Content/' + info.view">
             <div class="card bg-dark text-white fw-bold CardInfo">
-                <img :src="getImage(info.img)" class="card-img" style="height: 20rem;">
+                <img :src="peopleimg" class="card-img" style="height: 20rem;">
                 <div class="card-img-overlay" style="background-color: rgba(0, 0, 0, .5);">
                     <h3 class="card-title fw-bold">{{ info.title }}</h3>
                     <p class="card-text">{{ info.content }}</p>
