cf066ca918560d36e9218b386f7c363effaa76de
Kevin <kevin22413180@gmail.com>
2024-02-26 19:19:09 +0800

    vite 靜態檔 路徑測試

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,8 @@ import HomeCardData from '@/JsonData/HomeCardData.js'
 import { RouterLink } from 'vue-router'
 
 const getImage = (img) => {
-    const imageUrl = new URL('../Images/' + img, import.meta.url).href;
     console.log(import.meta.url);
-    console.log(imageUrl);
-    return imageUrl;
+    return new URL('../Images/' + img, import.meta.url).href;;
 }
 </script>
 
