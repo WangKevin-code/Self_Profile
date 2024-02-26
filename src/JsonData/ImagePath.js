cc221b2876a90a45b73e0d365f080257e9aa442d
Kevin <kevin22413180@gmail.com>
2024-02-26 20:32:38 +0800

    vite 靜態檔 路徑測試5

---
+++ src/JsonData/ImagePath.js
@@ -0,0 +1,18 @@
+import calculator from '@/Images/calculator.jpg'
+import dashborad from '@/Images/dashborad.jpg'
+import DataTransfer from '@/Images/DataTransfer.jpg'
+import Form from '@/Images/Form.jpg'
+import office2 from '@/Images/office2.jpg'
+import people from '@/Images/people.jpg'
+import temperature from '@/Images/temperature.jpg'
+import TodoList from '@/Images/TodoList.jpg'
+export const imgPath = {
+    "calculator.jpg": calculator,
+    "dashborad.jpg": dashborad,
+    "DataTransfer.jpg": DataTransfer,
+    "Form.jpg": Form,
+    "office2.jpg": office2,
+    "people.jpg": people,
+    "temperature.jpg": temperature,
+    "TodoList.jpg": TodoList,
+}
